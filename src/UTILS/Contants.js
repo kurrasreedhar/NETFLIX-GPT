e19accd5fcd2ad3ca90762769dd4021fa606a668
kurra sreedhar <arrukjay888@gmail.com>
2024-10-10 16:28:51 +0530

    removed

--- src/UTILS/Contants.js
+++ src/UTILS/Contants.js
@@ -11,4 +11,4 @@ export const options = {
     {identifier:"hindi",name:"Hindi"},
     {identifier:"spanish",name:"Spanish"}]
 
-    export const OPENAI_API_KEY='AIzaSyD28qAeQAwTVzm_eyCErn4vGEPJo8kLlI4'
+    export const OPENAI_API_KEY=''
